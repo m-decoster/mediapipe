4e5952295eeebda8a0321ffddfbb9ada2cf4dffb
Mathieu De Coster <mth.decoster@gmail.com>
2020-01-21 14:03:32 +0100

    Add support for outputting hand landmarks to a text file

---
+++ mediapipe/examples/desktop/hand_tracking_extract_gpu.cc
@@ -0,0 +1,151 @@
+// Copyright 2019 The MediaPipe Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// An example of sending OpenCV webcam frames into a MediaPipe graph.
+// This example requires a linux computer and a GPU with EGL support drivers.
+
+#include "mediapipe/framework/calculator_framework.h"
+#include "mediapipe/framework/formats/image_frame.h"
+#include "mediapipe/framework/formats/image_frame_opencv.h"
+#include "mediapipe/framework/port/commandlineflags.h"
+#include "mediapipe/framework/port/file_helpers.h"
+#include "mediapipe/framework/port/opencv_highgui_inc.h"
+#include "mediapipe/framework/port/opencv_imgproc_inc.h"
+#include "mediapipe/framework/port/opencv_video_inc.h"
+#include "mediapipe/framework/port/parse_text_proto.h"
+#include "mediapipe/framework/port/status.h"
+#include "mediapipe/gpu/gl_calculator_helper.h"
+#include "mediapipe/gpu/gpu_buffer.h"
+#include "mediapipe/gpu/gpu_shared_data_internal.h"
+#include <fstream>
+
+constexpr char kInputStream[] = "input_video";
+constexpr char kOutputStream[] = "outputs";
+
+DEFINE_string(
+    calculator_graph_config_file, "",
+    "Name of file containing text format CalculatorGraphConfig proto.");
+DEFINE_string(input_video_path, "",
+              "Full path of video to load. "
+              "If not provided, attempt to use a webcam.");
+DEFINE_string(output_file_path, "",
+              "Full path of where to save result (.txt only). ");
+
+::mediapipe::Status RunMPPGraph() {
+  std::string calculator_graph_config_contents;
+  MP_RETURN_IF_ERROR(mediapipe::file::GetContents(
+      FLAGS_calculator_graph_config_file, &calculator_graph_config_contents));
+  LOG(INFO) << "Get calculator graph config contents: "
+            << calculator_graph_config_contents;
+  mediapipe::CalculatorGraphConfig config =
+      mediapipe::ParseTextProtoOrDie<mediapipe::CalculatorGraphConfig>(
+          calculator_graph_config_contents);
+
+  LOG(INFO) << "Initialize the calculator graph.";
+  mediapipe::CalculatorGraph graph;
+  MP_RETURN_IF_ERROR(graph.Initialize(config));
+
+  LOG(INFO) << "Initialize the GPU.";
+  ASSIGN_OR_RETURN(auto gpu_resources, mediapipe::GpuResources::Create());
+  MP_RETURN_IF_ERROR(graph.SetGpuResources(std::move(gpu_resources)));
+  mediapipe::GlCalculatorHelper gpu_helper;
+  gpu_helper.InitializeForTest(graph.GetGpuResources().get());
+
+  LOG(INFO) << "Initialize the camera or load the video.";
+  cv::VideoCapture capture;
+  const bool load_video = !FLAGS_input_video_path.empty();
+  if (load_video) {
+    capture.open(FLAGS_input_video_path);
+  } else {
+    capture.open(0);
+  }
+  RET_CHECK(capture.isOpened());
+
+  LOG(INFO) << "Start running the calculator graph.";
+  ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller poller,
+                   graph.AddOutputStreamPoller(kOutputStream));
+  MP_RETURN_IF_ERROR(graph.StartRun({}));
+
+  LOG(INFO) << "Start grabbing and processing frames.";
+  size_t frame_timestamp = 0;
+  bool grab_frames = true;
+  std::ofstream ofs; // Output file stream for the floats
+  ofs.open(FLAGS_output_file_path);
+  while (grab_frames) {
+    // Capture opencv camera or video frame.
+    cv::Mat camera_frame_raw;
+    capture >> camera_frame_raw;
+    if (camera_frame_raw.empty()) break;  // End of video.
+    cv::Mat camera_frame;
+    cv::cvtColor(camera_frame_raw, camera_frame, cv::COLOR_BGR2RGB);
+    if (!load_video) {
+      cv::flip(camera_frame, camera_frame, /*flipcode=HORIZONTAL*/ 1);
+    }
+
+    // Wrap Mat into an ImageFrame.
+    auto input_frame = absl::make_unique<mediapipe::ImageFrame>(
+        mediapipe::ImageFormat::SRGB, camera_frame.cols, camera_frame.rows,
+        mediapipe::ImageFrame::kGlDefaultAlignmentBoundary);
+    cv::Mat input_frame_mat = mediapipe::formats::MatView(input_frame.get());
+    camera_frame.copyTo(input_frame_mat);
+
+    // Prepare and add graph input packet.
+    MP_RETURN_IF_ERROR(
+        gpu_helper.RunInGlContext([&input_frame, &frame_timestamp, &graph,
+                                   &gpu_helper]() -> ::mediapipe::Status {
+          // Convert ImageFrame to GpuBuffer.
+          auto texture = gpu_helper.CreateSourceTexture(*input_frame.get());
+          auto gpu_frame = texture.GetFrame<mediapipe::GpuBuffer>();
+          glFlush();
+          texture.Release();
+          // Send GPU image packet into the graph.
+          MP_RETURN_IF_ERROR(graph.AddPacketToInputStream(
+              kInputStream, mediapipe::Adopt(gpu_frame.release())
+                                .At(mediapipe::Timestamp(frame_timestamp++))));
+          return ::mediapipe::OkStatus();
+        }));
+
+    // Get the graph result packet, or stop if that fails.
+    mediapipe::Packet packet;
+    if (!poller.Next(&packet)) break;
+    std::vector<float> output_floats = packet.Get<std::vector<float>>();
+
+    // Write to the output stream
+    // One line per frame
+    for (int fi = 0; fi < output_floats.size(); ++fi) {
+        ofs << output_floats[fi];
+        if (fi != output_floats.size() - 1) { // Skip last comma
+            ofs << ",";
+        }
+    }
+    ofs << std::endl;
+  }
+
+  LOG(INFO) << "Shutting down.";
+  ofs.close();
+  MP_RETURN_IF_ERROR(graph.CloseInputStream(kInputStream));
+  return graph.WaitUntilDone();
+}
+
+int main(int argc, char** argv) {
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  ::mediapipe::Status run_status = RunMPPGraph();
+  if (!run_status.ok()) {
+    LOG(ERROR) << "Failed to run the graph: " << run_status.message();
+  } else {
+    LOG(INFO) << "Success!";
+  }
+  return 0;
+}
