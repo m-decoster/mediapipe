3ac94e6008a97d5270393c8ec46e11792b629975
Mathieu De Coster <mth.decoster@gmail.com>
2020-01-21 16:36:27 +0100

    Allow loading image directories in hand_tracking_extract_gpu example, remove webcam abilities

--- mediapipe/examples/desktop/hand_tracking_extract_gpu.cc
+++ mediapipe/examples/desktop/hand_tracking_extract_gpu.cc
@@ -29,6 +29,9 @@
 #include "mediapipe/gpu/gpu_buffer.h"
 #include "mediapipe/gpu/gpu_shared_data_internal.h"
 #include <fstream>
+#include <sstream>
+#include <vector>
+#include <glob.h> // Only tested on Ubuntu!
 
 constexpr char kInputStream[] = "input_video";
 constexpr char kOutputStream[] = "outputs";
@@ -38,9 +41,31 @@ DEFINE_string(
     "Name of file containing text format CalculatorGraphConfig proto.");
 DEFINE_string(input_video_path, "",
               "Full path of video to load. "
-              "If not provided, attempt to use a webcam.");
+              "If not provided, looks at input_image_dir_path.");
+DEFINE_string(input_image_dir_path, "",
+              "Full path of directory of images to load. "
+              "If not provided, fails.");
 DEFINE_string(output_file_path, "",
               "Full path of where to save result (.txt only). ");
+
+// based on https://stackoverflow.com/questions/8401777/simple-glob-in-c-on-unix-system/8615450#8615450
+std::vector<std::string> glob(const std::string &pattern) {
+  glob_t glob_result = {0};
+  int ret = glob(pattern.c_str(), GLOB_TILDE, NULL, &glob_result);
+  if (ret) {
+    globfree(&glob_result);
+    std::stringstream ss;
+    ss << "glob() failed with return_value " << ret << std::endl;
+    throw std::runtime_error(ss.str());
+  }
+  std::vector<std::string> result;
+  result.reserve(glob_result.gl_pathc);
+  for(size_t i = 0; i < glob_result.gl_pathc; ++i) {
+    result.push_back(std::string(glob_result.gl_pathv[i]));
+  }
+  globfree(&glob_result);
+  return result;
+}
 
 ::mediapipe::Status RunMPPGraph() {
   std::string calculator_graph_config_contents;
@@ -62,15 +87,17 @@ DEFINE_string(output_file_path, "",
   mediapipe::GlCalculatorHelper gpu_helper;
   gpu_helper.InitializeForTest(graph.GetGpuResources().get());
 
-  LOG(INFO) << "Initialize the camera or load the video.";
+  LOG(INFO) << "Load the video or list files in the directory.";
   cv::VideoCapture capture;
+  std::vector<std::string> image_files;
   const bool load_video = !FLAGS_input_video_path.empty();
   if (load_video) {
     capture.open(FLAGS_input_video_path);
+    RET_CHECK(capture.isOpened());
   } else {
-    capture.open(0);
+    std::string suffix = FLAGS_input_image_dir_path.back() == '/' ? "*.png" : "/*.png";
+    image_files = glob(FLAGS_input_image_dir_path + suffix);
   }
-  RET_CHECK(capture.isOpened());
 
   LOG(INFO) << "Start running the calculator graph.";
   ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller poller,
@@ -82,15 +109,21 @@ DEFINE_string(output_file_path, "",
   bool grab_frames = true;
   std::ofstream ofs; // Output file stream for the floats
   ofs.open(FLAGS_output_file_path);
+  int frame_counter = 0;
   while (grab_frames) {
-    // Capture opencv camera or video frame.
-    cv::Mat camera_frame_raw;
-    capture >> camera_frame_raw;
-    if (camera_frame_raw.empty()) break;  // End of video.
     cv::Mat camera_frame;
-    cv::cvtColor(camera_frame_raw, camera_frame, cv::COLOR_BGR2RGB);
-    if (!load_video) {
-      cv::flip(camera_frame, camera_frame, /*flipcode=HORIZONTAL*/ 1);
+    if (load_video) {
+      // Capture opencv camera or video frame.
+      cv::Mat camera_frame_raw;
+      capture >> camera_frame_raw;
+      if (camera_frame_raw.empty()) break;  // End of video.
+      cv::cvtColor(camera_frame_raw, camera_frame, cv::COLOR_BGR2RGB);
+    } else {
+      if (frame_counter >= image_files.size()) break; // End of dir.
+      std::string filename = image_files.at(frame_counter);
+      cv::Mat camera_frame_raw = cv::imread(filename);
+      if (camera_frame_raw.empty()) break;  // For some reason we couldn't read this file.
+      cv::cvtColor(camera_frame_raw, camera_frame, cv::COLOR_BGR2RGB); 
     }
 
     // Wrap Mat into an ImageFrame.
@@ -130,6 +163,8 @@ DEFINE_string(output_file_path, "",
         }
     }
     ofs << std::endl;
+    
+    ++frame_counter;
   }
 
   LOG(INFO) << "Shutting down.";
